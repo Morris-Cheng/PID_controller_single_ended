01639e18673fe1550dc56813de4c1fb6605ae4f0
Morris Cheng <morris.yuyen.cheng@gmail.com>
2025-08-17 19:38:35 -0700

    Filter function removed

--- PI_controller_single_sided.srcs/sources_1/new/PI_controller_testbench.v
+++ PI_controller_single_sided.srcs/sources_1/new/PI_controller_testbench.v
@@ -27,21 +27,15 @@ module PI_controller_testbench();
     reg signed [20:0] reference_value;
     reg rst;
     wire signed [20:0] control_signal;
-    wire signed [20:0] filtered_signal;
-    wire signed [20:0] output_value;
-    reg filter;
 
     integer file;
 
     PI_controller uut(
-        .input_value(measured_height),
-        .reference_value(reference_value),
+        .measured_height(measured_height),
+        .reference_height(reference_value),
         .clk(clk),
         .rst(rst),
-        .filter_enable(filter),
-        .output_control_signal(control_signal),
-        .filtered_signal(filtered_signal),
-        .output_value(output_value)
+        .control_signal_output(control_signal)
     );
 
     // Clock generation (100 MHz)
@@ -59,15 +53,14 @@ module PI_controller_testbench();
         reference_value = 20;
         measured_height = 20;
         rst = 1;
-        filter = 1;
         #30;
         rst = 0;
         #100;
         measured_height = 200;
-//        #100;
-//        measured_height = 50;
-//        #100;
-//        measured_height = 100;        
+        #200;
+        measured_height = 50;
+        #200;
+        measured_height = 100;        
     end
 
     // File I/O
@@ -76,13 +69,13 @@ module PI_controller_testbench();
         file = $fopen("simulation_output_0811_with_filter.csv", "w");
 
         // Write CSV header
-        $fwrite(file, "measured_height, control_signal, filtered_value\n");
+        $fwrite(file, "measured_height, control_signal\n");
 
         // Write values every 10ns (on rising edge)
         @(negedge rst);  // wait for reset deassertion
         repeat (1000) begin
             @(posedge clk);
-            $fwrite(file, "%0d,%0d,%0d\n", measured_height, control_signal, filtered_signal);
+            $fwrite(file, "%0d,%0d\n", measured_height, control_signal);
         end
 
         // Close the file
